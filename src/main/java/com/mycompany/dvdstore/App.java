a5a9ab126b0562ee29a5f6c5af68fe47205c4e03
cca <cca@civadis.be>
2022-06-11 01:03:02 +0200

    injection par code dans app

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,7 +2,10 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.entity.Movie;
+import com.mycompany.dvdstore.repository.GoLiveMovieRepository;
+import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import com.mycompany.dvdstore.service.MovieService;
+import com.mycompany.dvdstore.service.MovieServiceInterface;
 
 import java.util.Scanner;
 
@@ -15,6 +18,10 @@ public class App
     public static void main( String[] args )
     {
         MovieController movieController = new MovieController();
+        MovieService movieService = new MovieService();
+        movieController.setMovieService(movieService);
+        GoLiveMovieRepository movieRepository = new GoLiveMovieRepository();
+        movieService.setMovieRepository(movieRepository);
         movieController.addUsingConsole();
     }
 }
