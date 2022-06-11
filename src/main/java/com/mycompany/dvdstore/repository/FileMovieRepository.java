7f3de5ebfedc344fd54fff83de8e0a465daf593c
ccml <claude.cato@proximus.be>
2022-06-11 21:34:49 +0200

    autowire bytype

--- src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
+++ src/main/java/com/mycompany/dvdstore/repository/FileMovieRepository.java
@@ -2,16 +2,23 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
 
 public class FileMovieRepository implements MovieRepositoryInterface {
+    private File file;
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
     public void add(Movie movie) {
         try {
             Writer output;
-            output = new BufferedWriter(new FileWriter("D:\\temp\\movie.txt", true));
+            output = new BufferedWriter(new FileWriter(file, true));
             output.append(movie.getTitle() + ";" + movie.getGenre() + "\n");
             output.flush();
             output.close();
