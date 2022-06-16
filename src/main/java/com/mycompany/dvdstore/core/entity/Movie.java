d284eea6dcf33ef88d67cf85e28be8a7065f41c0
ccml <claude.cato@proximus.be>
2022-06-16 13:56:20 +0200

    jdbc

--- src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
+++ src/main/java/com/mycompany/dvdstore/core/entity/Movie.java
@@ -6,6 +6,21 @@ public class Movie {
     private String genre;
     private String description;
 
+    public Movie() {}
+
+    public Movie(Long id, String title, String genre) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public Movie(Long id, String title, String genre, String description) {
+        this.id = id;
+        this.title = title;
+        this.genre = genre;
+        this.description = description;
+    }
+
     public String getTitle() {
         return title;
     }
---
+++ src/main/java/com/mycompany/dvdstore/core/repository/database/MovieRepository.java
@@ -0,0 +1,56 @@
+package com.mycompany.dvdstore.core.repository.database;
+
+import com.mycompany.dvdstore.core.entity.Movie;
+import com.mycompany.dvdstore.core.repository.MovieRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
+public class MovieRepository implements MovieRepositoryInterface {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public List<Movie> list() {
+        return jdbcTemplate.query(
+                "select ID, TITLE, GENRE from MOVIE",
+                (rs, rowNum) ->
+                        new Movie(rs.getLong("ID"), rs.getString("TITLE"), rs.getString("GENRE"))
+        );
+    }
+
+    @Override
+    public Movie getById(Long id) {
+        return jdbcTemplate.queryForObject(
+                "select ID, TITLE, GENRE, DESCRIPTION from MOVIE where ID=?",
+                new Object[] { id },
+                (rs, rowNum) ->
+                        new Movie(rs.getLong("ID"), rs.getString("TITLE"), rs.getString("GENRE"), rs.getString("DESCRIPTION"))
+        );
+    }
+
+    @Override
+    public Movie add(Movie movie) {
+        KeyHolder kh=new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement pstmt=connection.prepareStatement("INSERT INTO MOVIE (TITLE,GENRE,DESCRIPTION) VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
+            pstmt.setString(1,movie.getTitle());
+            pstmt.setString(2,movie.getGenre());
+            pstmt.setString(3,movie.getDescription());
+            return pstmt;
+        },kh);
+
+        movie.setId(kh.getKey().longValue());
+
+        return movie;
+    }
+}
